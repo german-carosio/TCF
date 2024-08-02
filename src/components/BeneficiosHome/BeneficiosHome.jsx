32cdc1f5812416e7e871b84eca13114aca35c301
German Carosio <GCAROSIO@claro.com.ar>
2024-08-02 12:03:58 -0300

    mejoras css

--- src/components/BeneficiosHome/BeneficiosHome.jsx
+++ src/components/BeneficiosHome/BeneficiosHome.jsx
@@ -6,13 +6,16 @@ const BeneficiosHome = () => {
   return (
     <section className={styles.container}>
       <div className={styles.container2}>
-      <div>
-        <h2 className={styles.title}>SÚPERALIMENTO: <br /> BENEFICIOS DE LOS <br /> FRIJOLES</h2>
-        <Boton title={'Ver beneficios'} url={'/benefits'} />
-      </div>
-      <div> 
-        <p>Los beneficios de los frijoles son tantos que son considerados un superalimento. Aquí podrás descubrir todos los beneficios de los frijoles para tu salud.</p>
-      </div>
+        <div className={styles.lado1}>
+            <h2 className={styles.title}>Súperalimento: <br /> Beneficios de los frijoles</h2> 
+            
+            <Boton title={'Ver beneficios'} url={'/benefits'} textColor="#fff"
+            borderColor="#fff" /> 
+            </div>      
+        
+        <div className={styles.lado2}>
+          <p>Los beneficios de los frijoles son tantos que son considerados un superalimento. Aquí podrás descubrir todos los beneficios de los frijoles para tu salud.</p>
+        </div>
       </div>
     </section>
   )
